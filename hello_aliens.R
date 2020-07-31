18ef53a652916d20f28a6e9286b8cbe0a3cf5082
MarkCoopershlyak <mcoopers@andrew.cmu.edu>
2020-07-30 20:16:29 -0400

    This is also for the R Project, Q17 to be specific.

--- hello_aliens.R
+++ hello_aliens.R
@@ -1,1 +1,16 @@
-first_sent = 
+
+# This is for question 17 of the R Project for 36-350
+
+sentences  = c("Dear Aliens,", "\n",
+               " You must be running out of things to read if you are reading this;
+                if I am still around, I could recommend some good books.",
+               " This github repo was created for a class that I am taking, so
+                unless you find R code interesting, you should turn away.",
+               " While you are here, however, I have a few questions that I
+                would like to ask.",
+               " Firstly, did you guys make Stonehenge or the pyramind in Egypt?",
+               " I always heard that those ancient guys got some help, so I 
+                wasn't sure if it was you guys or someone else.",
+               " Second question: do you guys live on the moon?",
+               " If so, did you see some astronauts in the 1960's and 70's?",
+               " You guys should have taken a group photo or something.")
